46afc56bb959331650b461840bad8df33fec6c8e
Orcun Karakaya <karakayaorcun100@gmail.com>
2021-04-24 16:56:10 -0400

    added more wor after publishing to github

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Adding GitHub Now");
-        System.out.println("Yay Stuff are on Github");
+        System.out.println("YAY STUFFS ARE ON GITHUB");
+        // adding final wrap up code.
+        System.out.println("");
 
     }
 }
